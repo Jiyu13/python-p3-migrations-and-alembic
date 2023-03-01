9d84a79e4329e44c4a4e8f01963408f0f1f8b031
Ziru Zou <jiyucoding@yahoo.com>
2023-02-28 21:10:24 -0500

    code along

---
+++ lib/migrations/versions/79eeedceeebd_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 79eeedceeebd
+Revises: 
+Create Date: 2023-02-28 20:08:08.289855
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '79eeedceeebd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
